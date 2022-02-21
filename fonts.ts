5bf7baeb22b46bdd1bd11d9b9c57bd35d910f9e7
Kristoffer Vinther <kristoffer@zliide.com>
2022-02-21 15:42:31 +0100

    Support fonts loaded from memory.

--- fonts.ts
+++ fonts.ts
@@ -1,18 +1,16 @@
 class FontFace {
     readonly family
-    readonly asset
-    readonly descriptors
+    readonly source
+    readonly descriptors?
+
     #status: 'error' | 'loaded' | 'loading' | 'unloaded' = 'loading'
     get status() {
         return this.#status
     }
 
-    constructor(family: string, source: any, descriptors: string) {
-        if (typeof source !== 'string') {
-            throw new Error('Not implemented.')
-        }
+    constructor(family: string, source: string | Uint8Array, descriptors?: string) {
         this.family = family
-        this.asset = source
+        this.source = source
         this.descriptors = descriptors
     }
 
@@ -30,6 +28,7 @@ export function installFonts(window: any) {
 export function scriptLoadingFonts(window: any) {
     return 'const fontLoaders = [];\r\n' + [...window.document.fonts]
         .filter(face => !face.family.startsWith('\'')) // Ignore Safari work-around
-        .map((face, ix) => `const _f${ix} = new FontFace('${face.family}','${face.asset}',${JSON.stringify(face.descriptors)});\r\nfontLoaders.push(_f${ix}.load().then(function() { document.fonts.add(_f${ix}); }).catch(function (e) { console.error('Error loading font ${face.family}'); console.error(e); }));`)
+        .map((face: FontFace, ix) =>
+            `const _f${ix} = new FontFace('${face.family}',${typeof face.source === 'string' ? '\'' + face.source + '\'' : 'Buffer.from(\'' + Buffer.from(face.source).toString('base64') + '\', \'base64\')'},${JSON.stringify(face.descriptors)});\r\nfontLoaders.push(_f${ix}.load().then(function() { document.fonts.add(_f${ix}); }).catch(function (e) { console.error('Error loading font ${face.family}'); console.error(e); }));`)
         .join('\r\n')
 }
